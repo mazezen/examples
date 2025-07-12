139834dd930ceb7b00c34255b28e37e9d0818edd
mazezen <mazezen24@gmail.com>
2025-07-12 19:32:44 +0800

    Delete nodejs.org directory

--- nodejs.org/documentation/task_processor.js
+++
@@ -1,4 +0,0 @@
-const { parentPort } = require("node:worker_threads");
-parentPort.on("message", (task) => {
-  parentPort.postMessage(task.a + task.b);
-});
--- nodejs.org/learn/streams.mjs
+++
@@ -1,9 +0,0 @@
-import { createReadStream, createWriteStream } from "node:fs";
-import { pipeline } from "node:stream/promises";
-import { createGzip } from "node:zlib";
-
-await pipeline(
-  createReadStream("package.json"),
-  createGzip(),
-  createWriteStream("package.json")
-);
